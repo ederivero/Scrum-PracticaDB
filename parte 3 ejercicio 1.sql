47bf33b71338d919a26b302cd7b0c3d3fa9063a5
jo3l17 <joelvaldezangeles@gmail.com>
2019-04-16 12:52:40 -0500

    Create parte 3 ejercicio 1.sql

---
+++ parte 3 ejercicio 1.sql
@@ -0,0 +1,8 @@
+/*parte 3 ejercicio 1*/
+select concat(anyo_inicio,"-",anyo_fin) as periodo,id_asignatura,c.nombre as materia,per.nombre as nombre_de_profesor 
+from alumno_se_matricula_asignatura as a
+left join periodo_academico as b on a.id_periodo_academico=b.id
+left join asignatura as c on b.id=c.id
+left join profesor as p on c.id_profesor=p.id_profesor
+left join persona as per on per.id = p.id_profesor
+where a.id_periodo_academico=1
